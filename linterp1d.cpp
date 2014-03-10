b913c0200d89c8ff45a8a53035d79c090e11a045
Greg von Winckel <greg.von.winckel@gmail>
2014-03-10 00:08:37 -0600

    first commit

---
+++ linterp1d.cpp
@@ -0,0 +1,51 @@
+#include <iostream>     // std::cout
+#include <algorithm>    // std::lower_bound, std::upper_bound
+#include <vector>       // std::vector
+
+
+//class LinearInterp {
+//
+//};
+
+
+
+int main () {
+
+  int m = 11;
+  int n = 21; 
+
+  std::vector<double>::iterator low,up;
+
+  std::vector<double> x1(m);
+  std::vector<double> x2(n);
+
+  std::vector<int> lower(n);
+  std::vector<int> upper(n);
+
+  int k;
+
+
+  for(k=0;k<m;++k)
+  {
+     x1[k] = k/double(m-1);
+  }
+
+  for(k=0;k<n;++k)
+  {
+     x2[k] = k/double(n-1);
+  }
+
+
+   for(k=0;k<n;++k)
+   {
+    
+      low = std::lower_bound(x1.begin(),x1.end(),x2[k]);
+      up = std::upper_bound(x1.begin(),x1.end(),x2[k]);
+      lower[k] = (low-x1.begin());
+      upper[k] = (up-x1.begin());
+      std::cout << lower[k] << " " << upper[k] << std::endl;
+
+    } 
+
+  return 0;
+}
